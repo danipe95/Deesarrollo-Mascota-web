0f54d58fef9cf2ac2e2739da62846785949eaf3c
[Ameliacmi] <[crissmuigle30@hotmail.com]>
2021-10-05 12:26:47 -0500

    cambios en la base de datos

--- Mascoticas.App/Mascoticas.App.Persistencia/AppRepositorios/AppContext.cs
+++ Mascoticas.App/Mascoticas.App.Persistencia/AppRepositorios/AppContext.cs
@@ -6,13 +6,15 @@ namespace Mascoticas.App.Persistencia
     public class AppContext : DbContext
     {
         public DbSet<Persona> Personas {get;set;}
+        public DbSet<Mascota> Mascotas {get;set;}
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         if (!optionsBuilder.IsConfigured)
         {
             optionsBuilder
-            .UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = MascoticastData");
+            .UseSqlServer(@"Data Source=localhost\SQLEXPRESS;Initial Catalog = MascotasData;Integrated Security = True");
+            //.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initia  l Catalog = MascoticastData ");
         }
     }
     }
--- Mascoticas.App/Mascoticas.App.Persistencia/Migrations/20210918050316_Inicial.cs
+++ Mascoticas.App/Mascoticas.App.Persistencia/Migrations/20210918050316_Inicial.cs
@@ -22,11 +22,34 @@ namespace Mascoticas.App.Persistencia.Migrations
                     table.PrimaryKey("PK_Personas", x => x.Id);
                 });
         }
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Mascota",
+                columns: table => new
+                {
+                    IdMascota = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Tipo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Raza = table.Column<int>(type: "int", nullable: true),
+                    Propietario = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Peso = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Temperatura = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Propietario = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mascota", x => x.Id);
+                });
+        }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
                 name: "Personas");
+                migrationBuilder.DropTable(
+                name: "Mascotas");
         }
     }
 }
