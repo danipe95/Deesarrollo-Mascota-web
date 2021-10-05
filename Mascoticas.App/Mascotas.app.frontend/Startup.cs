27ef5ec5b2abe91c0276a4ba0172b26f5ed3758a
[Ameliacmi] <[crissmuigle30@hotmail.com]>
2021-10-05 15:37:43 -0500

    repositorios actualizados

--- Mascoticas.App/Mascotas.app.frontend/Startup.cs
+++ Mascoticas.App/Mascotas.app.frontend/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Mascoticas.App.Persistencia;
+using Mascoticas.App.Persistencia.AppRepositorios;
 
 namespace Mascotas.app.frontend
 {
@@ -24,6 +26,8 @@ namespace Mascotas.app.frontend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSingleton<IRepositorioMascota, RepositorioMascota>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
