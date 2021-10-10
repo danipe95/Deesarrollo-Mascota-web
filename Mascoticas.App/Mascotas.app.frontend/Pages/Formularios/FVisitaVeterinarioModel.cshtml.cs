1329ff9351a0c2873fc17ef012027cd098727a25
[Ameliacmi] <[crissmuigle30@hotmail.com]>
2021-10-10 14:51:18 -0500

    consola

--- Mascoticas.App/Mascotas.app.frontend/Pages/Formularios/FVisitaVeterinarioModel.cshtml.cs
+++ Mascoticas.App/Mascotas.app.frontend/Pages/Formularios/FVisitaVeterinarioModel.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Mascoticas.App.Dominio;
+using Mascoticas.App.Persistencia.AppRepositorios;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +11,16 @@ namespace Mascotas.app.frontend.pages
 {
     public class FVisitaVeterinarioModelModel : PageModel
     {
+        private readonly IRepositorioVeterinario repositorioVeterinario;
+        public IEnumerable<Veterinario> Veterinarios {get;set;} 
+
+        public  FVisitaVeterinarioModelModel(IRepositorioVeterinario repositorioVeterinario)
+        {
+            this.repositorioVeterinario = new RepositorioVeterinario(new Mascoticas.App.Persistencia.AppContext());
+        }
         public void OnGet()
         {
-            
+            Veterinarios = repositorioVeterinario.GetAll();
         }
     }
 }
